4d1874b72617446feba84da34ed87673b43c6f70
JamesTLopez <JTlopez919@gmail.com>
2018-11-22 12:19:29 -0500

    Edit string names

--- front-end/src/Actions/types.js
+++ front-end/src/Actions/types.js
@@ -7,4 +7,5 @@ export const FETCH_HALLS = 'FETCH_HAllS';
 export const ADD_FOOD = 'ADD_FOOD';
 export const DELETE_FOOD = 'DELETE_FOOD';
 export const RESET_FOOD = 'RESET_FOOD';
+export const FETCH_FOOD = 'FETCH_FOOD';
 export const FETCH_FOODS = 'FETCH_FOODS';
--- front-end/src/Reducers/foodReducer.js
+++ front-end/src/Reducers/foodReducer.js
@@ -1,18 +1,18 @@
 
-import { ADD_HALL, DELETE_HALL, FETCH_HALL, RESET_HALL,FETCH_HALLS } from './../Actions/types.js';
+import { ADD_FOOD, DELETE_FOOD, FETCH_FOOD, RESET_FOOD,FETCH_FOODS } from './../Actions/types.js';
 
 export default function hallReducer(state = {}, action) {
     switch (action.type) {
-        case RESET_HALL:
-            return {...state, createStatus:false, hall:{}};
-        case ADD_HALL:
-            return {...state, createStatus:true, hall: action.payload.id};
-        case DELETE_HALL:
+        case RESET_FOOD:
+            return {...state, createStatus:false, food:{}};
+        case ADD_FOOD:
+            return {...state, createStatus:true, food: action.payload.id};
+        case DELETE_FOOD:
             return state.filter(post => post._id !== action.payload.id);
-        case FETCH_HALL:
-            return {...state, hall: action.payload};
-        case FETCH_HALLS:
-            return {...state, halls: action.payload};
+        case FETCH_FOOD:
+            return {...state, food: action.payload};
+        case FETCH_FOODS:
+            return {...state, foods: action.payload};
         default:
             return state;
     }
