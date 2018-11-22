1e463f5c14f6579599b3f8e47a0cced8037f8bc4
kumaran <ragunat@uwindsor.ca>
2018-11-22 17:03:26 -0500

    Update AddFoodOrder.js

--- front-end/src/components/AddFoodOrder.js
+++ front-end/src/components/AddFoodOrder.js
@@ -37,26 +37,13 @@ class AddFoodOrder extends Component {
             <Grid container spacing={24} style={{padding:0, margin:0, width:"100%"}}>
 
               <Grid item xs={3}>
-                <Typography align="center" variant="h4" gutterBottom>
-                  Add Food Order
-                </Typography>
+                <Paper>
+                    Food details
+                </Paper>
               </Grid>
 
               <Grid item xs={3}>
-                <FormControl variant="filled">
-                <InputLabel>Food</InputLabel>
-                <Select native
-                  value={this.state.food}
-                  input={<FilledInput name="food"
-                  id="food"
-                  onChange={this.handleChange.bind(this, "food")} />}
-                  >
-                  <option value="" />
-                  <option value={10}>Food a</option>
-                  <option value={20}>Food b</option>
-                  <option value={30}>Food c</option>
-                  </Select>
-                </FormControl>
+
               </Grid>
 
               <Grid item xs={3}>
